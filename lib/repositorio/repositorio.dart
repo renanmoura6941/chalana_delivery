8814caede3e88e8a041e80bc210b6737aa5c1807
Isaias_Santos <isaias661248@gmail.com>
2020-10-10 16:05:55 -0300

    repositorio

---
+++ lib/repositorio/repositorio.dart
@@ -0,0 +1,8 @@
+import 'package:chalana_delivery/modelos/produto_modelo.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Repositorio {
+  List<ProdutoModelo> listaProdutos() {
+    // Firestore.instance.collection(path)
+  }
+}
