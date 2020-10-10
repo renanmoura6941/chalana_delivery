03718aabd85bf84ac45c43a117ecd45c973cede3
RenanMoura14 <naneraruom@gmail.com>
2020-10-10 15:02:15 -0300

    modelo produto

--- lib/modelos/produto_modelo.dart
+++ lib/modelos/produto_modelo.dart
@@ -1,1 +1,18 @@
-class Produto {}
+class Produto {
+  String nome;
+  String descrissao;
+  List<String> imagens;
+  double preco;
+  int acessos;
+  String categorias;
+
+  Produto(this.nome, this.preco,this.descrissao,this.imagens,this.acessos,this.categorias);
+
+
+  @override
+  String toString() {
+    return "nome: $nome, descrissao: $descrissao,preco: $preco acessos: $acessos, categorias: $categorias, imagens: $imagens";
+  }
+
+
+}
