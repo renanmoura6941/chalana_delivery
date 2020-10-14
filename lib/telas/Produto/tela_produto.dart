bd9eb8dccfac24a51b40573bec0761390da14787
RenanMoura14 <naneraruom@gmail.com>
2020-10-14 08:56:40 -0300

    lista de imagens tela de produto

--- lib/telas/Produto/tela_produto.dart
+++ lib/telas/Produto/tela_produto.dart
@@ -1,8 +1,30 @@
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 
 class TelaProduto extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    List<String> imagens = [
+      "https://delivery.supermuffato.com.br/arquivos/ids/258950-1000-1000/7897395099329.jpg?v=637127241430430000",
+      "https://www.imigrantesbebidas.com.br/bebida/images/products/full/222_Cerveja_Heineken_Long_Neck_330_ml.jpg"
+         ];
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Cerveja"), centerTitle: true,),
+      body: ListView(
+        children: [
+          AspectRatio(
+            aspectRatio: 1,
+            child: Carousel(
+              dotSize: 4,
+              //dotBgColor: Colors.transparent,
+              autoplay: false,
+                images: imagens.map((url) {
+              return NetworkImage(url);
+            }).toList()),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/telas/principal/componentes/card.dart
+++ lib/telas/principal/componentes/card.dart
@@ -4,7 +4,9 @@ class CardPrincipal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: (){},
+      onTap: (){
+        
+      },
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.start,
