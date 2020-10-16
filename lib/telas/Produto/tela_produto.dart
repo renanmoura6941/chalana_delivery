f5892a6ffa7a0c29fbd5859434ff677d12a1182d
RenanMoura14 <renan.moura.silva.06@aluno.ifce.edu.br>
2020-10-16 14:10:00 -0300

    tela produto

--- lib/telas/Produto/tela_produto.dart
+++ lib/telas/Produto/tela_produto.dart
@@ -4,24 +4,73 @@ import 'package:flutter/material.dart';
 class TelaProduto extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    
     List<String> imagens = [
       "https://delivery.supermuffato.com.br/arquivos/ids/258950-1000-1000/7897395099329.jpg?v=637127241430430000",
       "https://www.imigrantesbebidas.com.br/bebida/images/products/full/222_Cerveja_Heineken_Long_Neck_330_ml.jpg"
-         ];
+    ];
 
     return Scaffold(
-      appBar: AppBar(title: Text("Cerveja"), centerTitle: true,),
+      appBar: AppBar(
+        title: Text("Cerveja"),
+        centerTitle: true,
+      ),
       body: ListView(
         children: [
           AspectRatio(
             aspectRatio: 1,
             child: Carousel(
-              dotSize: 4,
+              dotSize: 6,
               //dotBgColor: Colors.transparent,
               autoplay: false,
-                images: imagens.map((url) {
-              return NetworkImage(url);
-            }).toList()),
+              images: imagens.map((url) {
+                return NetworkImage(url);
+              }).toList(),
+              dotIncreasedColor: Colors.blue,
+              dotBgColor: Colors.transparent,
+              dotColor: Colors.blue,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "product.name",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8),
+                  child: Text(
+                    'A partir de',
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                      fontSize: 13,
+                    ),
+                  ),
+                ),
+                Text(
+                  'R\$ 19.99',
+                  style: TextStyle(
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold,
+                    // color: primaryColor,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16, bottom: 8),
+                  child: Text(
+                    'Descrição',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                Text(
+                  "product.description",
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
           )
         ],
       ),
