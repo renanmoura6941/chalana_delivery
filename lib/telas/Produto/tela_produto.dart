a58a5b43718027ff99dee2b03b885e5a2789280c
RenanMoura14 <renan.moura.silva.06@aluno.ifce.edu.br>
2020-10-17 19:07:05 -0300

    tela Produto

--- lib/telas/Produto/tela_produto.dart
+++ lib/telas/Produto/tela_produto.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 class TelaProduto extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
     List<String> imagens = [
       "https://delivery.supermuffato.com.br/arquivos/ids/258950-1000-1000/7897395099329.jpg?v=637127241430430000",
       "https://www.imigrantesbebidas.com.br/bebida/images/products/full/222_Cerveja_Heineken_Long_Neck_330_ml.jpg"
@@ -69,6 +68,23 @@ class TelaProduto extends StatelessWidget {
                   "product.description",
                   style: const TextStyle(fontSize: 16),
                 ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  
+                  width: double.infinity,
+                  height: 45,
+                  child: RaisedButton(
+                    color: Colors.blue,
+                    child: Text("Adicionar ao carrinho", style: TextStyle(
+                      fontSize: 18
+                    ),),
+                    onPressed: (){
+
+                    },
+                  ),
+                )
               ],
             ),
           )
