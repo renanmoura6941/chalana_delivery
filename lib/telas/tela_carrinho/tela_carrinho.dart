d2a71d5c6d8ac59a1482396c62b1434ca939c5e7
Isaias_Santos <isaias661248@gmail.com>
2020-10-28 20:46:36 -0300

    carrinho

--- lib/telas/tela_carrinho/tela_carrinho.dart
+++ lib/telas/tela_carrinho/tela_carrinho.dart
@@ -1,8 +1,8 @@
 import 'package:chalana_delivery/modelos/pedido_modelo.dart';
+import 'package:chalana_delivery/telas/tela_carrinho/controller_carrinho.dart';
 
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'components/card_produto_carrinho.dart';
 import 'components/confirm_button_carrinho.dart';
 
@@ -13,6 +13,7 @@ class TelaCarrinho extends StatefulWidget {
 
 class _TelaCarrinhoState extends State<TelaCarrinho> {
   List<PedidoModelo> list_produto = [];
+  final controller = ControllerCarriho();
 
   @override
   void initState() {
@@ -44,46 +45,7 @@ class _TelaCarrinhoState extends State<TelaCarrinho> {
                 if (index == list_produto.length) {
                   return ConfirmButtonCar(
                     onPressed: () async {
-                      print("Confirmar pedido");
-                      var produto = {
-                        'nome': "Cerveja da boa",
-                        'quantidade': 4,
-                        'unidade': 2.00,
-                        'valor': 8.00
-                      };
-                      var produto2 = {
-                        'nome': "vinho especial",
-                        'quantidade': 10,
-                        'unidade': 2.00,
-                        'valor': 20.00
-                      };
-
-                      List<Map> dados = [produto, produto2];
-
-                      String q = "%0A";
-                      String divisor = "------------------------------------";
-                      double total = 0;
-                      String mensagem = "Minha solicitação:$q";
-                      for (var e in dados) {
-                        mensagem +=
-                            "$divisor$q*Produto*: ${e['nome']}$q*Quantidade*: ${e['quantidade']}$q*Preço da unidade*: R\$ ${e['unidade'].toStringAsFixed(2)}$q*Valor*: R\$ ${e['valor'].toStringAsFixed(2)}$q";
-                        total += e['valor'];
-                      }
-
-                      mensagem += divisor +
-                          q +
-                          q +
-                          q +
-                          "*Preço total da solicitação*: ${total.toStringAsFixed(2)}";
-
-                      String url =
-                          "https://api.whatsapp.com/send/?phone=5585992954232&text=${mensagem}&app_absent=0";
-
-                      try {
-                        await launch(url);
-                      } catch (erro) {
-                        debugPrint("erro ao enviar mensagem: $erro");
-                      }
+                      await controller.confirmarPedido();
                     },
                   );
                 }
