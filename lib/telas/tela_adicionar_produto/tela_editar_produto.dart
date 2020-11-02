ceabd2f66c525fee5dcac6ebfdd5601bd6ce4fe9
naneraruom@gmail.com <renan.moura.silva06@aluno.ifce.edu.br>
2020-11-02 02:29:16 -0300

    add imagem local

--- lib/telas/tela_adicionar_produto/tela_editar_produto.dart
+++ lib/telas/tela_adicionar_produto/tela_editar_produto.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:carousel_pro/carousel_pro.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -12,33 +13,69 @@ class TelaAdicionarProduto extends StatefulWidget {
 class _TelaAdicionarProdutoState extends State<TelaAdicionarProduto> {
   File _image;
   final picker = ImagePicker();
+  List produtoImagensLocal;
 
-  Future _imgFromCamera() async {
-    final pickedFile = await picker.getImage(source: ImageSource.camera);
+  Future<void> _recuperarImagem(StorageTaskSnapshot snapshot) async {
+    String url = await snapshot.ref.getDownloadURL();
+    print(url);
+  }
 
-    setState(() {
-      if (pickedFile != null) {
-        _image = File(pickedFile.path);
-      } else {
-        print('No image selected.');
+  Future _uploadImagem() async {
+    //referencia o arquivo
+    FirebaseStorage storage = FirebaseStorage.instance;
+    StorageReference pastaraiz = storage.ref();
+    StorageReference arquivo = pastaraiz.child("fotos").child("foto.jpg");
+
+    StorageUploadTask task = arquivo.putFile(_image);
+
+    task.events.listen((event) {
+      if (event.type == StorageTaskEventType.progress) {
+        print("em progresso");
+      } else if (event.type == StorageTaskEventType.failure) {
+        print("falha na foto");
+      } else if (event.type == StorageTaskEventType.success) {
+        print("sucesso na foto");
       }
+    });
+
+    task.onComplete.then((StorageTaskSnapshot snapshot) {
+      _recuperarImagem(snapshot);
     });
   }
 
-  Future _imgFromGallery() async {
-    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+  Future _imagenCamera() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
 
     if (pickedFile != null) {
       _image = File(pickedFile.path);
       setState(() {
-        _image = File(pickedFile.path);
+        produtoImagensLocal.add(Image.file(_image));
       });
     } else {
       print('No image selected.');
     }
   }
 
-  void _showPicker(context) {
+  Future _imagenGaleria() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      _image = File(pickedFile.path);
+      setState(() {
+        produtoImagensLocal.add(Image.file(_image));
+      });
+    } else {
+      print('No image selected.');
+    }
+  }
+
+  remover() {
+    setState(() {
+      produtoImagensLocal.removeLast();
+    });
+  }
+
+  void _adicionar(context) {
     showModalBottomSheet(
         context: context,
         builder: (BuildContext bc) {
@@ -50,14 +87,14 @@ class _TelaAdicionarProdutoState extends State<TelaAdicionarProduto> {
                       leading: Icon(Icons.photo_library),
                       title: Text('Galeria'),
                       onTap: () {
-                        _imgFromGallery();
+                        _imagenGaleria();
                         Navigator.of(context).pop();
                       }),
                   ListTile(
                     leading: Icon(Icons.photo_camera),
                     title: Text('Camera'),
                     onTap: () {
-                      _imgFromCamera();
+                      _imagenCamera();
                       Navigator.of(context).pop();
                     },
                   ),
@@ -69,9 +106,13 @@ class _TelaAdicionarProdutoState extends State<TelaAdicionarProduto> {
   }
 
   @override
-  Widget build(BuildContext context) {
-   
+  void initState() {
+    produtoImagensLocal = [];
+    super.initState();
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Adicionar produto"),
@@ -86,22 +127,15 @@ class _TelaAdicionarProdutoState extends State<TelaAdicionarProduto> {
                 Carousel(
                   dotSize: 6,
                   autoplay: false,
-                  images: [
-                    AspectRatio(
-                      aspectRatio: 1,
-                      child: Container(
-                        color: Colors.grey,
-                        child: Center(
-                          child: _image == null
-                              ? Icon(
-                                  Icons.photo,
-                                  size: 100,
-                                )
-                              : Image.file(_image),
-                        ),
-                      ),
-                    ),
-                  ],
+                  images: produtoImagensLocal.isEmpty
+                      ? [
+                          Center(
+                              child: Icon(
+                            Icons.photo,
+                            size: 100,
+                          ))
+                        ]
+                      : produtoImagensLocal,
                   dotIncreasedColor: Colors.blue,
                   dotBgColor: Colors.transparent,
                   dotColor: Colors.blue,
@@ -109,27 +143,35 @@ class _TelaAdicionarProdutoState extends State<TelaAdicionarProduto> {
                 Positioned(
                     bottom: 1,
                     right: 1,
-                    child: InkWell(
-                      child: CircleAvatar(
-                        radius: 30,
-                        child: Icon(Icons.photo_camera),
-                      ),
-                      onTap: () {
-                        _showPicker(context);
-                      },
+                    child: Row(
+                      children: [
+                        if (produtoImagensLocal.isNotEmpty)
+                          Positioned(
+                              bottom: 1,
+                              right: 1,
+                              child: InkWell(
+                                child: CircleAvatar(
+                                  radius: 30,
+                                  child: Icon(Icons.remove),
+                                ),
+                                onTap: () {
+                                  remover();
+                                },
+                              )),
+                        InkWell(
+                          child: CircleAvatar(
+                            radius: 30,
+                            child: Icon(Icons.photo_camera),
+                          ),
+                          onTap: () {
+                            _adicionar(context);
+                          },
+                        ),
+                      ],
                     ))
               ],
             ),
           ),
-          // AspectRatio(
-          //   aspectRatio: 1,
-          //   child: Container(
-          //     color: Colors.blue,
-          //     child: Center(
-          //       child: _image == null ? Icon(Icons.photo, size: 100,) : Image.file(_image),
-          //     ),
-          //   ),
-          // ),
           Padding(
             padding: const EdgeInsets.all(16),
             child: Column(
