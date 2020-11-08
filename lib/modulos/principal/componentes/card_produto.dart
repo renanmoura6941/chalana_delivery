09749425df63c98092df4c71050b088ef3068760
naneraruom@gmail.com <renan.moura.silva06@aluno.ifce.edu.br>
2020-11-08 09:08:03 -0300

    home

--- lib/modulos/principal/componentes/card_produto.dart
+++ lib/modulos/principal/componentes/card_produto.dart
@@ -10,7 +10,7 @@ class CardProduto extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: ()  {
+      onTap: () {
         Navigator.pushNamed(context, "produto", arguments: produto.copiar());
       },
       // child: Card(
